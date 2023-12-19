9233d766d4908e97a7d386b31889c4ed432c3cb3
devosher01 <devosher01@gmail.com>
2023-12-19 12:41:44 -0500

    Refactor Skills component and extract SkillsIcon and Skills_Container

--- src/pages/home/components/Skills.jsx
+++ src/pages/home/components/Skills.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Marquee from 'react-fast-marquee';
-import { FaHtml5, FaCss3, FaJs, FaReact, FaNodeJs } from 'react-icons/fa';
-import { SiTailwindcss, SiFlutter, SiDart, SiGo, SiPostman } from 'react-icons/si';
+import skills from '../config/skillsConfig';
 
 const SkillItem = ({ skill, color, textColor, Icon }) => (
     <div className={`flex items-center gap-2 p-2 rounded-full m-2 ${color}`}>
@@ -20,33 +19,22 @@ const SkillsMarquee = ({ skills }) => (
     </Marquee>
 );
 
-const Skills = () => {
-    const skills = [
-        { name: 'HTML', color: 'bg-red-500', textColor: 'text-white', Icon: FaHtml5 },
-        { name: 'CSS', color: 'bg-blue-500', textColor: 'text-white', Icon: FaCss3 },
-        { name: 'JavaScript', color: 'bg-yellow-300', textColor: 'text-black', Icon: FaJs },
-        { name: 'React', color: 'bg-blue-700', textColor: 'text-white', Icon: FaReact },
-        { name: 'Node.js', color: 'bg-green-600', textColor: 'text-white', Icon: FaNodeJs },
-        { name: 'Tailwind', color: 'bg-indigo-500', textColor: 'text-white', Icon: SiTailwindcss },
-        { name: 'Flutter', color: 'bg-blue-400', textColor: 'text-white', Icon: SiFlutter },
-        { name: 'Dart', color: 'bg-blue-600', textColor: 'text-white', Icon: SiDart },
-        { name: 'Rest API', color: 'bg-red-600', textColor: 'text-white', Icon: SiPostman },
-        { name: 'Golang', color: 'bg-blue-300', textColor: 'text-black', Icon: SiGo },
-    ];
+const SkillsIcon = () => (
+    <svg viewBox="0 0 256 256" className="h-4 w-4 mr-2" fill="#45A29E">
+        <path d="M235.33,116.72,139.28,20.66a16,16,0,0,0-22.56,0l-96,96.06a16,16,0,0,0,0,22.56l96.05,96.06h0a16,16,0,0,0,22.56,0l96.05-96.06a16,16,0,0,0,0-22.56ZM128,224h0L32,128,128,32,224,128Z"></path>
+    </svg>
+);
 
-    return (
-        <div className="sm:w-[800px] w-full h-auto max-w-full bg-bg-container flex flex-col items-center justify-center rounded-[12px] p-4">
-            <div className="flex items-center mb-2 w-full">
-                <svg viewBox="0 0 256 256" className="h-4 w-4 mr-2" fill="#45A29E">
-                    <path d="M235.33,116.72,139.28,20.66a16,16,0,0,0-22.56,0l-96,96.06a16,16,0,0,0,0,22.56l96.05,96.06h0a16,16,0,0,0,22.56,0l96.05-96.06a16,16,0,0,0,0-22.56ZM128,224h0L32,128,128,32,224,128Z"></path>
-                </svg>
-                <h2 className="text-white">Skills</h2>
-            </div>
-            <div className="flex flex-col items-center overflow-hidden w-full">
-                <SkillsMarquee skills={skills} />
-            </div>
+const Skills_Container = () => (
+    <div className="sm:w-[800px] w-full h-auto max-w-full bg-bg-container flex flex-col items-center justify-center rounded-[12px] p-4">
+        <div className="flex items-center mb-2 w-full">
+            <SkillsIcon />
+            <h2 className="text-white">Skills</h2>
         </div>
-    );
-};
+        <div className="flex flex-col items-center overflow-hidden w-full">
+            <SkillsMarquee skills={skills} />
+        </div>
+    </div>
+);
 
-export default Skills;
+export default Skills_Container;
