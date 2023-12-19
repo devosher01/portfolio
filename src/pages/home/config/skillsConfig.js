6ba4c702e1967f962a986bd0b8952e48b9b76ccb
devosher01 <devosher01@gmail.com>
2023-12-19 12:42:45 -0500

    Add skills configuration file

---
+++ src/pages/home/config/skillsConfig.js
@@ -0,0 +1,17 @@
+import * as Icons from 'react-icons/fa';
+import * as SiIcons from 'react-icons/si';
+
+const skills = [
+    { name: 'HTML', color: 'bg-red-500', textColor: 'text-white', Icon: Icons.FaHtml5 },
+    { name: 'CSS', color: 'bg-blue-500', textColor: 'text-white', Icon: Icons.FaCss3 },
+    { name: 'JavaScript', color: 'bg-yellow-300', textColor: 'text-black', Icon: Icons.FaJs },
+    { name: 'React', color: 'bg-blue-700', textColor: 'text-white', Icon: Icons.FaReact },
+    { name: 'Node.js', color: 'bg-green-600', textColor: 'text-white', Icon: Icons.FaNodeJs },
+    { name: 'Tailwind', color: 'bg-indigo-500', textColor: 'text-white', Icon: SiIcons.SiTailwindcss },
+    { name: 'Flutter', color: 'bg-blue-400', textColor: 'text-white', Icon: SiIcons.SiFlutter },
+    { name: 'Dart', color: 'bg-blue-600', textColor: 'text-white', Icon: SiIcons.SiDart },
+    { name: 'Rest API', color: 'bg-red-600', textColor: 'text-white', Icon: SiIcons.SiPostman },
+    { name: 'Golang', color: 'bg-blue-300', textColor: 'text-black', Icon: SiIcons.SiGo },
+];
+
+export default skills;
